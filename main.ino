74728fbca90f66a1a37fcf28670420d6a5ec8305
Jason <jolson@udel.edu>
2019-09-23 19:12:30 -0500

    Update main.ino
    Replaced Adafruit Motor Shield library with general instructions for L298N motor control bridge.

--- main.ino
+++ main.ino
@@ -1,61 +1,80 @@
-#include <AFMotor.h>
+//successfully driving one wheel so far
 
-AF_DCMotor port(2); // create motor #2
-AF_DCMotor starboard(1); // create motor #1
-AF_DCMotor weapon(3); //create spinning weapon motor
-int time = 100;
+// Program: Control 2 DC motors using L298N H Bridge
+
+// Definitions Arduino pins connected to input H Bridge
+int IN1 = 8;
+int IN2 = 7;
+
+int IN3 = 9;
+int IN4 = 10;
+
+int time = 1000;
 int speed = 255;
 int direction = 0;
 
 void setup()
 {
- Serial.begin(9600);
- Serial.println("<Arduino is ready>");
-
-  port.setSpeed(speed);     // set the speed to 200/255
-  starboard.setSpeed(speed);     // set the speed to 200/255
-  weapon.setSpeed(255);
+ // Set the output pins
+ pinMode(IN1, OUTPUT);
+ pinMode(IN2, OUTPUT);
+ pinMode(IN3, OUTPUT);
+ pinMode(IN4, OUTPUT);
+}
+  
+void loop()
+{
+  go(direction);
+  delay(time);
+  direction = random(-1,2);
 }
 
-void loop() {
-weapon.run(FORWARD);
-go(direction);
-direction = random(-1,2);
+void leftForward()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
 }
 
-void go(int) {
-switch (direction) {
+void rightForward()
+{
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void leftBackward()
+{
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+}
+
+void rightBackward()
+{
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void go(int direction)
+{
+switch(direction)
+{
   case -1:
   //backwards
-  port.run(RELEASE);
-  starboard.run(RELEASE);
-  port.run(BACKWARD);
-  starboard.run(BACKWARD);
-  delay(time);
+  leftBackward();
+  rightBackward();
   break;
   case 0:
   //forwards
-  port.run(RELEASE);
-  starboard.run(RELEASE);
-  port.run(FORWARD);
-  starboard.run(FORWARD);
-  delay(time);
+  leftForward();
+  rightForward();
   break;
   case 1:
-  //rotate left
-  port.run(RELEASE);
-  starboard.run(RELEASE);
-  port.run(FORWARD);
-  starboard.run(BACKWARD);
-  delay(time);
+  //left
+  leftBackward();
+  rightForward();
   break;
   case 2:
-  //rotate right
-  port.run(RELEASE);
-  starboard.run(RELEASE);
-  port.run(BACKWARD);
-  starboard.run(FORWARD);
-  delay(time);
+  leftForward();
+  rightBackward();
   break;
-}
+  }
 }
